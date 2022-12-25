147319f70f7625d70eec0ae9a691eee38cbacee1
Koma077 <Sladkoeshka33@yandex.ru>
2022-12-26 00:25:30 +0700

    zxccv

--- src/main/java/com/skypro/springbasket/controller/CartController.java
+++ src/main/java/com/skypro/springbasket/controller/CartController.java
@@ -1,4 +1,27 @@
 package com.skypro.springbasket.controller;
 
+import com.skypro.springbasket.service.CartService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
 public class CartController {
+    private final CartService cartService;
+
+    public CartController(CartService cartService){
+        this.cartService = cartService;
+    }
+    @GetMapping("/add")
+    public void add(@RequestParam("id") List<Integer> ids) {
+        this.cartService.addToCart(ids);
+    }
+    @GetMapping("/get")
+    public List<Integer> get(){
+        return this.cartService.getCart();
+    }
 }
